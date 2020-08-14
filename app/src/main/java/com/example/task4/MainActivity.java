d7f026e93ebde322815ff32832e076a9170adf34
vjmarcus <vjmarcus@list.ru>
2020-08-14 14:37:41 +0300

    Add map

--- app/src/main/java/com/example/task4/MainActivity.java
+++ app/src/main/java/com/example/task4/MainActivity.java
@@ -5,16 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.task4.ui.custom.CustomCircleView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        button = findViewById(R.id.goToMapButton);
     }
 
 }
