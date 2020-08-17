0a360ad0f7fb116c55136067a15dfd1faf71cb1e
vjmarcus <vjmarcus1@gmail.com>
2020-08-17 14:23:05 +0300

    Update Api key

--- app/src/main/java/com/example/task4/MapsActivity.java
+++ app/src/main/java/com/example/task4/MapsActivity.java
@@ -2,7 +2,6 @@ package com.example.task4;
 
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
--- app/src/main/java/com/example/task4/ui/custom/CustomCircleView.java
+++ app/src/main/java/com/example/task4/ui/custom/CustomCircleView.java
@@ -3,19 +3,12 @@ package com.example.task4.ui.custom;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
-
 import androidx.annotation.Nullable;
-
 import com.example.task4.R;
-
-import java.util.Random;
 
 public class CustomCircleView extends View {
 
